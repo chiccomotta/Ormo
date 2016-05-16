9f3e15b93b60ec510d9182aa6f59a3553a12895a
chiccomotta <cristiano.motta@hotmail.it>
2016-05-16 21:53:53 +0200

    Add files via upload

---
+++ TableMapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CM.Ormo
+{
+    /***
+        Attributo per impostare il nome della tabella sulla clasee
+    ***/
+    [System.AttributeUsage(System.AttributeTargets.Class, AllowMultiple = false)]
+    public class TableMapper : System.Attribute
+    {
+        private readonly string tableName;
+
+        public TableMapper(string tableName)
+        {
+            this.tableName = tableName;
+        }
+
+        public string GetTableName()
+        {
+            return this.tableName;
+        }
+    }
+}
